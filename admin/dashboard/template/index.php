810a765169a89402aeb92ac0507b91025a6571cd
Marco Luis Hernandez <marcoluis.hernandez@gmail.com>
2023-11-03 11:46:56 +0800

    11/3/2023

--- admin/dashboard/template/index.php
+++ admin/dashboard/template/index.php
@@ -78,6 +78,7 @@ if(isset($_POST["submit"])){
     
 <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="icon" href="images/logajade.png" type="image/x-icon">
   <title>Southern Jade Life Insurance</title>
     <!-- Font Awesome -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
--- applicant/applicant_page/index.php
+++ applicant/applicant_page/index.php
@@ -622,6 +622,7 @@ $userFolder = 'documents/' . $foldername . '/';
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>Southern Jade</title>
+  <link rel="icon" href="../../images/logajade.png" type="image/x-icon">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha384-ePjGJxNV59PKnOMIwJ0Dp0a7KeW2R4zj9Ukb8UaRKY1exaP0gNz6roF26djpvt0A" crossorigin="anonymous" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
