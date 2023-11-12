ed4f3382fbc098915d0a0c7362e461709676d31b
Marco Luis Hernandez <marcoluis.hernandez@gmail.com>
2023-11-12 20:03:35 +0800

    11/12/2023

--- registration/index.php
+++ registration/index.php
@@ -214,7 +214,7 @@ $crslerror = '';
                       </h3>
                       
                       <p>
-                        <a href='index.php'><button class="button button--success" data-for="js_success-popup">OK</button></a>
+                        <a href='../index.php'><button class="button button--success" data-for="js_success-popup">OK</button></a>
                       </p>
                     </div>
                   </div>
